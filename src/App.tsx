e4cc174b0a2776cc8883420126a599f6a2ece67b
Oleh Tatarynov <exocriador@gmail.com>
2025-05-02 14:02:33 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import AppRouter from "./routes/AppRouter";
+
+function App() {
+  return (
+    <div className="font-sans">
+      <AppRouter />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Dashboard.tsx
@@ -0,0 +1,10 @@
+const Dashboard = () => {
+  return (
+    <div className="p-10">
+      <h2 className="text-2xl font-semibold">Кабінет користувача</h2>
+      <p>Тут будуть ваші нагадування і календар.</p>
+    </div>
+  );
+};
+
+export default Dashboard;
